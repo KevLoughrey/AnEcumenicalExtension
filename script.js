34792fdea5c0b97b313e5854cd492a8bd8703ea2
Kevin Loughrey <kevloughrey@gmail.com>
2021-04-06 23:20:30 +0100

    style: add background and font to quotes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var _URL = `https://api.fatherted.irish/quotes/random`;
-var _TARGET_ELEMENT = document.getElementById("dynamic");
+var _TARGET_ELEMENT = document.getElementById("quote");
 
 fetch(_URL)
     .then(response => response.json())
