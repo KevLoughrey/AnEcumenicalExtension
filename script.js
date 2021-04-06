efcbebd57e2755be0cc330a5ee4c8ce2234f9016
Kevin Loughrey <kevloughrey@gmail.com>
2021-04-06 21:57:12 +0100

    feat: retrieve and display unformatted api quote

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-document.getElementById("dynamic").innerHTML = "Hello, World";
+var _URL = `https://api.fatherted.irish/quotes/random`;
+var _TARGET_ELEMENT = document.getElementById("dynamic");
+
+fetch(_URL)
+    .then(response => response.json())
+    .then(data => _TARGET_ELEMENT.innerHTML = parseData(data));
+
+function parseData(obj) {
+    return obj.quote;
+}
